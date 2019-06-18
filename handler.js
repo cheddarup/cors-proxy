61a492b21b7830dd194e17b21209a97170e966bd
Kareem Janoudi <kjanoudi@gmail.com>
2019-06-18 16:21:15 -0400

    rewrite it

--- handler.js
+++ handler.js
@@ -1,67 +1,38 @@
-'use strict';
+const p = require("phin");
 
-const request = require('request');
+module.exports.corsProxy = async (event, context) => {
+  let params = event.queryStringParameters;
+  console.log(`Got request:`, event);
 
-/**
- * Use this command to launch the handler from console:
- *
- * node_modules/.bin/serverless invoke local -f lambda -d '{"httpMethod":"GET","queryStringParameters":{"url":"http://github.com"}}'
- *
- *  or from browser
- *
- * http://localhost:3000/?url=https://github.com
- */
-module.exports.corsProxy = (event, context, callback) => {
-    let params = event.queryStringParameters;
+  if (!params.url) {
+    return {
+      statusCode: 400,
+      body: "Unable get url from 'url' query parameter"
+    };
+  }
 
-    console.log(event);
-    console.log(`Got request with params:`, params);
-
-    if (!params.url) {
-        const errorResponse = {
-            statusCode: 400,
-            body: 'Unable get url from \'url\' query parameter'
-        };
-
-        callback(null, errorResponse);
-
-        return;
-    }
-
-    return new Promise((resolve, reject) => {
-        let originalRequestBody = event.body;
-        request({
-            url: params.url,
-            method: event.httpMethod,
-            timeout: 20000,
-            json: event.httpMethod === 'POST' ? JSON.parse(originalRequestBody) : null,
-        }, (err, originalResponse, body) => {
-            if (err) {
-                console.log(`Got error`, err);
-
-                callback(err);
-
-                reject(err);
-
-                return;
-            }
-
-            console.log(`Got response from ${params.url} ---> {statusCode: ${originalResponse.statusCode}}`);
-            const proxyBody = originalRequestBody ? JSON.stringify(body) : originalResponse.body;
-
-            const proxyResponse = {
-                statusCode: originalResponse.statusCode,
-                headers: {
-                    "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-                    "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-                    "content-type": originalResponse.headers['content-type']
-                },
-                body: proxyBody
-            };
-
-            callback(null, proxyResponse);
-
-            resolve(proxyResponse);
-        });
+  try {
+    const res = await p({
+      url: params.url,
+      method: event.httpMethod,
+      data: event.body ? JSON.parse(event.body) : null,
+      timeout: 20000
     });
-};
+    const response = {
+      statusCode: res.statusCode,
+      headers: {
+        "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+        "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
+        "content-type": res.headers["content-type"]
+      },
+      body: res.body
+    };
+    return response;
+  } catch (err) {
+    console.log(`Got error`, err);
+    return {
+      statusCode: 400,
+      body: err.msg
+    };
+  }
+}
